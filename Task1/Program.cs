9061c1222268c36513eb014a3fe5ed7baf750b15
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-06-17 12:29:01 +0300

    Add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+//int a = 5;
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+//int b  = 25;
+int square = a * a;
+
+if (square == b)
+Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
