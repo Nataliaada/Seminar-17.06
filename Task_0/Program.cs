54e0043c8e41cb06c52cc10f0b9fdbe115f356c1
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-06-17 11:32:41 +0300

    Добавили задачу про сумму

---
+++ Task_0/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine(true);
+
+// byte bt = 125; //2^8
+// int i = 5; // 2^32
+// double d = 5.5; //2^64
+// string s = "Привет !";
+// bool b = false;
+
+// double a = 4;
+// double x = 5;
+// double i2 = a/x;
+// Console.WriteLine(i2);
+
+// int  a = 4;
+// int x = 2022;
+// double i2 = a/x;
+// byte bt1 = (byte)x;
+// int x1 = bt;
+// Console.WriteLine(x1);
+
+// Демонтсрация решения
+// Напишите программу которая на вход
+
+Console.Write("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+// int num = 5;
+int square = num * num;
+Console.Write($"Квадрат числа {num}: "); // Console.Write($"Квадрат числа {num} = {square}");
+Console.WriteLine(square);
+
+
+
+
