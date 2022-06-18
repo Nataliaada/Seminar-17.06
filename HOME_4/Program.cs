e91c7169f9048b4a85d00524363e416a67c1e2a8
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-06-18 21:42:38 +0300

    Add Home 4

---
+++ HOME_4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа 
+//от 1 до N. 5 -> 2, 48 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= b);
+ i = i +1;
+if (i % 2 ==0) Console.WriteLine($"{i}");
+else Console.WriteLine();
